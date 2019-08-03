a2a34b07fa9bac25b793f9bd9f1701bd357bf38c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-03 19:10:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+
+  attr_accessor :name
+  # class variable
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+
+end
